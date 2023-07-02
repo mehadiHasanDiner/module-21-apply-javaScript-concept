bcdf81a1e61957b5695177e9c1110b5672efe6d9
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-03 00:49:39 +0600

    recap js

---
+++ recap.js
@@ -0,0 +1,13 @@
+// 1. variable
+/***
+     * যেগুলোতে সিঙ্গাল ডাটা থাকে সেগুলো premitive type যেমন- integer, string, boolean
+     * আর যেগুলোতে মিক্স ডাটা থাকে সেগুলো non-premitive যেমন - Array, object/ 
+     
+    *  */
+var myName = "Khan";
+
+// 2. array
+// 3. conditionals
+// 4.loop (for loop, while loop)
+// 5. function
+// 6. object
---
+++ var-let-const.js
@@ -0,0 +1,6 @@
+let age = 11;
+age = 12;
+
+const countryName = "United States";
+// countryName = "United kingdom";
+console.log(countryName);
